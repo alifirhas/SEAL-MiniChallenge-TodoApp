672cd2a732c679520870abf871c0578348db1385
alifirhas <irhasalif@gmail.com>
2022-08-10 08:52:35 +0700

    :triangular_flag_on_post: Auth user pada task
    Masih belum dry pada decoded token

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,24 +1,35 @@
 const db = require("../models/index");
 const Task = db.tasks;
-const {Op}= require("sequelize");
+const { Op } = require("sequelize");
+const jsonwebtoken = require("jsonwebtoken");
 
 class TaskController {
 	async get(req, res) {
 		try {
-            let filter = {}
-            
-            if(req.query.id){
-                filter.id = req.query.id
-            }
-            if(req.query.status){
-                filter.status = req.query.status
-            }
-            if(req.query.due_date){
-                filter.due_date = {[Op.gte]: req.query.due_date}
-            }
-            if(req.query.start_date && req.query.end_date){
-                filter.due_date = {[Op.between]: [req.query.start_date, req.query.end_date]}
-            }
+			const authToken = req.headers.authorization;
+			const decoded = jsonwebtoken.verify(
+				authToken.split(" ")[1],
+				process.env.SECRET_KEY
+			);
+
+			let filter = {
+				userId: decoded.user.id,
+			};
+
+			if (req.query.id) {
+				filter.id = req.query.id;
+			}
+			if (req.query.status) {
+				filter.status = req.query.status;
+			}
+			if (req.query.due_date) {
+				filter.due_date = { [Op.gte]: req.query.due_date };
+			}
+			if (req.query.start_date && req.query.end_date) {
+				filter.due_date = {
+					[Op.between]: [req.query.start_date, req.query.end_date],
+				};
+			}
 
 			const data = await Task.findAll({
 				where: filter,
@@ -30,7 +41,7 @@ class TaskController {
 				task: data,
 			});
 		} catch (error) {
-            console.log(error)
+			console.log(error);
 			res.status(400).send({
 				status: 400,
 				message: "Terjadi masalah dalam pengambilan data task",
@@ -41,12 +52,19 @@ class TaskController {
 
 	async post(req, res) {
 		try {
+			const authToken = req.headers.authorization;
+			const decoded = jsonwebtoken.verify(
+				authToken.split(" ")[1],
+				process.env.SECRET_KEY
+			);
+
 			const action = await Task.create({
 				name: req.body.name,
 				desc: req.body.desc,
 				status: 0,
 				due_date: req.body.due_date,
 				due_time: req.body.due_time,
+				userId: decoded.user.id,
 			});
 
 			res.status(200).send({
@@ -65,6 +83,12 @@ class TaskController {
 
 	async put(req, res) {
 		try {
+			const authToken = req.headers.authorization;
+			const decoded = jsonwebtoken.verify(
+				authToken.split(" ")[1],
+				process.env.SECRET_KEY
+			);
+
 			const action = await Task.update(
 				{
 					name: req.body.name,
@@ -76,6 +100,7 @@ class TaskController {
 				{
 					where: {
 						id: req.params.id,
+						userId: decoded.user.id,
 					},
 				}
 			);
@@ -96,6 +121,12 @@ class TaskController {
 
 	async delete(req, res) {
 		try {
+			const authToken = req.headers.authorization;
+			const decoded = jsonwebtoken.verify(
+				authToken.split(" ")[1],
+				process.env.SECRET_KEY
+			);
+
 			const action = await Task.destroy({
 				where: {
 					id: req.params.id,
