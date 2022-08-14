29b63526db8288ed5413e96f6fd2dbb8f04c22c2
Alvian Ghifari <alvian2002286@gmail.com>
2022-08-14 23:36:29 +0700

    add router for subtask update and delete

--- routes/SubTaskRouter.js
+++ routes/SubTaskRouter.js
@@ -4,5 +4,7 @@ const subTaskRouter = express.Router();
 const subTaskController = require("../controllers/SubTaskController");
 
 subTaskRouter.post("/", subTaskController.addSubTask);
+subTaskRouter.put("/:id", subTaskController.editSubTask);
+subTaskRouter.delete("/:id", subTaskController.deleteSubTask);
 
 module.exports = { subTaskRouter };
