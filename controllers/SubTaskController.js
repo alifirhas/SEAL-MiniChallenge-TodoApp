83495c9cb64ae566e900d421dbfdc5d1474b074e
Alvian Ghifari <alvian2002286@gmail.com>
2022-08-14 23:32:50 +0700

    fix addSubTask, add editSubTask and deleteSubTask function

--- controllers/SubTaskController.js
+++ controllers/SubTaskController.js
@@ -4,30 +4,102 @@ const subTask = db.subtask;
 
 exports.addSubTask = async (req, res, next) => {
     try {
-        const { name, desc, taskId } = req.body;
+        const { name, desc, due_date, due_time, taskId } = req.body;
 
         if (!name || !desc || !taskId) {
             return res.sendStatus(400);
         }
 
-        await subTask.create({
+        action = await subTask.create({
             name: name,
             desc: desc,
+            due_date: due_date,
+            due_time: due_time,
             taskId: taskId,
         });
 
-        res.status(201).json({
+        return res.status(201).json({
             status: 201,
-            message: "SubTask Created"
+            message: "SubTask Created",
+            subTask: action,
         });
 
     } catch (e) {
         console.log(e);
-        res.status(400).send({
+        return res.status(400).json({
             status: 400,
-            message: "Terjadi masalah dalam pengambilan data task",
+            message: "Terjadi masalah dalam pembuatan subtask",
             error: e,
         });
     }
 
 };
+
+exports.editSubTask = async (req, res, next) => {
+    try {
+        const { name, desc, status, due_date, due_time } = req.body;
+
+        const action = await subTask.update(
+            {
+                name: name,
+                desc: desc,
+                status: status,
+                due_date: due_date,
+                due_time: due_time
+            },
+            {
+                where: {
+                    id: req.params.id
+                },
+            }
+        );
+
+        if (action[0] === 0) {
+            return res.status(400).json({
+                status: 400,
+                message: "SubTask tidak ditemukan",
+            });
+        } else {
+            return res.status(200).json({
+                status: 200,
+                message: "SubTask berhasil diperbarui"
+            });
+        }
+
+    } catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: "Terjadi masalah dalam pembaruan subtask",
+            error: error,
+        });
+    }
+};
+
+exports.deleteSubTask = async (req, res, next) => {
+    try {
+        const action = await subTask.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (action[0] === 0) {
+            return res.status(400).json({
+                status: 400,
+                message: "SubTask tidak ditemukan",
+            });
+        } else {
+            return res.status(200).json({
+                status: 200,
+                message: "SubTask berhasil dihapus",
+            });
+        }
+
+    } catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: "Terjadi masalah dalam penghapusan task",
+            error: error,
+        });
+    }
+}
